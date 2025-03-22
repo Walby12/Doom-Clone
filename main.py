8233a60020680808a4109f5817007edbb936ce64
Walby12 <alby09.bonf@gmail.com>
2025-03-22 23:53:25 +0100

    Added a new compiler

--- main.py
+++ main.py
@@ -4,31 +4,34 @@ from map import Map
 from player import *
 from ray_caster import *
 
-pygame.init()
-screen = pygame.display.set_mode((WINDOW_W, WINDOW_H))
-pygame.display.set_caption("Raycaster Engine")
-clock = pygame.time.Clock()
+def start_new_ray_caster(window_h, window_w, title):
+    pygame.init()
+    screen = pygame.display.set_mode((window_h, window_w))
+    pygame.display.set_caption(title)
+    clock = pygame.time.Clock()
 
-game_map = Map()
-player = Player()
-ray_caster = Raycaster(player)
+    game_map = Map()
+    player = Player()
+    ray_caster = Raycaster(player)
 
-running = True
-while running:
-    screen.fill(BLACK)
+    running = True
+    while running:
+        screen.fill(BLACK)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    player.update()
+        player.update()
 
-    game_map.render(screen)
-    player.render(screen)
-    ray_caster.cast_all_rays()
-    ray_caster.render(screen)
+        game_map.render(screen)
+        player.render(screen)
+        ray_caster.cast_all_rays()
+        ray_caster.render(screen)
 
-    pygame.display.flip()
-    clock.tick(FPS)
+        pygame.display.flip()
+        clock.tick(FPS)
 
-pygame.quit()
+    pygame.quit()
+
+start_new_ray_caster(WINDOW_W, WINDOW_H, "Ray Caster engine")
